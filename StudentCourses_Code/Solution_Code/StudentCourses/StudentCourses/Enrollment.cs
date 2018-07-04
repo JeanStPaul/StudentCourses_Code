7efcf9e32119ba8fe923ac9cfc1678a6f0eac8be
Gymrat1958 <jeandstpaul@yahoo.com>
2018-07-04 18:49:28 -0400

    Student Courses

---
+++ StudentCourses_Code/Solution_Code/StudentCourses/StudentCourses/Enrollment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentCourses
+{
+    public class Enrollment
+    {
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+        public int Grade { get; set; }
+    }
+}
